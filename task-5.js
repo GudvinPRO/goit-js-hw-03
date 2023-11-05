825395dd0a0b62965877d6e87fd64c7f7c9760ad
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2023-11-05 14:26:05 +0200

    1

---
+++ task-5.js
@@ -0,0 +1,13 @@
+function calculateTax(amount, taxRate = 0.2) {
+
+    return amount * taxRate;
+ 
+}
+
+
+  console.log(calculateTax(100, 0.1)); 
+  console.log(calculateTax(200, 0.1)); 
+  console.log(calculateTax(200)); 
+
+
+
